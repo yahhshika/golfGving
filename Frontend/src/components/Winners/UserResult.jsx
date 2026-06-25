4c6bd6253002e84b8dc53b236468a3965c27d1f7
yahhshika <soniyashika1619@gmail.com>
2026-06-26 03:17:27 +0530

    fixed a small bug in UserResult.jsx

--- Frontend/src/components/Winners/UserResult.jsx
+++ Frontend/src/components/Winners/UserResult.jsx
@@ -9,6 +9,15 @@ export default function UserResult({winners}) {
   const winner = winners.find(
     (winner) => winner.userId?._id?.toString() === user?._id?.toString()
   );
+  const prize =
+  winner?.matchType === "3-match"
+    ? 0.25 * winner.prizeAmount
+    : winner?.matchType === "4-match"
+    ? 0.35 * winner.prizeAmount
+    : winner?.matchType === "5-match"
+    ? 0.4 * winner.prizeAmount
+    : null;
+  
   return (
     <section className="relative overflow-hidden rounded-2xl border border-yellow-500/20 bg-white/5 backdrop-blur-xl">
       
@@ -26,7 +35,7 @@ export default function UserResult({winners}) {
         </h1>
 
         <h2 className="mt-4 text-2xl md:text-4xl font-extrabold uppercase tracking-widest bg-gradient-to-r from-yellow-300 via-yellow-100 to-yellow-300 bg-clip-text text-transparent">
-          {winner?`Yay!! You Won ${(winner?.matchType==="3-match" && 0.25*winner?.prizeAmount).toLocaleString("en-IN")|| (winner?.matchType==="4-match" && 0.35*winner?.prizeAmount).toLocaleString("en-IN") || (winner?.matchType==="5-match" && 0.4*winner?.prizeAmount).toLocaleString("en-IN") || "not known"}`:"Sorry! Better Luck next time."}
+          {winner?`Yay!! You Won ${prize || "not known"}`:"Sorry! Better Luck next time."}
          
         </h2>
         {winner && 
